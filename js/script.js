23c4abccbee846e2b08c414fb19d8787d1429fa5
q-scaerou <q.scaerou@digital-box.fr>
2021-03-24 12:28:31 +0100

    updated PF

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', init);
+document.getElementById('cgshow').addEventListener('click', showCGjs);
+
+function init() {
+    console.log("DOM has been loaded successfully")
+}
+
+function showCGjs() {
+    img = document.getElementById('CGexample');
+    if (img.classList.contains('imgHide')) {
+        img.classList.remove('imgHide');
+        img.classList.add('imgShow');
+    } else {
+        img.classList.add('imgHide');
+        img.classList.remove('imgShow');
+    }
+
+}
